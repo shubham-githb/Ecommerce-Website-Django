64adc2b35053a3830ee5156ed69ba60f1ae55b25
shubham <subhamaother@gmail.com>
2021-03-23 11:01:04 +0530

    Initial commit

---
+++ bartbox/shop/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Product
+from django.http import HttpResponse
+from math import ceil
+
+# Create your views here.
+def index(request):
+    products= Product.objects.all()
+    allProds=[]
+    catprods= Product.objects.values('category', 'id')
+    cats= {item["category"] for item in catprods}
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params={'allProds':allProds }
+    return render(request,"shop/index.html", params)
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+def search(request):
+    return render(request, 'shop/search.html')
+
+def productView(request):
+    return render(request, 'shop/prodView.html')
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
